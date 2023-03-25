e71cabe72046231b37a85d7d5c64f46b281be28c
Parth Patel <123parth@pm.me>
2023-03-25 11:53:16 +0530

    rm initial setup

--- initialsetup/exa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo curl -L https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip | unzip -q exa-linux-x86_64-v0.10.1.zip bin/exa -d ~/.local/bin
--- initialsetup/gnomekey.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gsettings set org.gnome.desktop.input-sources sources "[('"xkb"','"g"'),('"xkb"','"us"')]"
-gsettings set org.gnome.desktop.input-sources current 1
